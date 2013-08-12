f2c4ec9bc9973e728a9c0b18bfb42df1dcc67705
Parin Porecha <parinporecha@gmail.com>
2013-08-12 18:00:00 +0530

    some changes were meant for Django < 1.3. So, reverting back

--- GTGOnline/settings.py
+++ GTGOnline/settings.py
@@ -133,7 +133,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.csrf.CsrfResponseMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
