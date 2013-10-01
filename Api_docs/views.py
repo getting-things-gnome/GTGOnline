dc3de3067f9fef7739ba9ff5a654c3cdff480066
Parin Porecha <parinporecha@gmail.com>
2013-10-01 16:56:19 +0530

    set manually access control headers in response object. Lets see if it works

--- Api_docs/views.py
+++ Api_docs/views.py
@@ -11,4 +11,12 @@ def resource_listing(request):
     json_data = open(CURRENT_PATH + '/resource.json')
     #data1 = json.load(json_data) // deserialises it
     #data2 = json.dumps(json_data)  #json formatted string
-    return HttpResponse(json_data, mimetype='application/json')
+    resp = HttpResponse()
+    resp['Access-Control-Allow-Origin'] = '*'
+    resp['Access-Control-Max-Age'] = '120'
+    resp['Access-Control-Allow-Credentials'] = 'true'
+    resp['Access-Control-Allow-Methods'] = 'HEAD, GET, OPTIONS, POST, DELETE'
+    resp['Access-Control-Allow-Headers'] = 'origin, content-type, accept, x-requested-with'
+    resp['mimetype'] = 'application/json'
+    resp.content = json_data
+    return resp
