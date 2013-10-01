971bdd9159f93f6e044357c8274b8e4ed640c757
Parin Porecha <parinporecha@gmail.com>
2013-10-01 16:49:08 +0530

    trying to make swagger work

--- Api_docs/views.py
+++ Api_docs/views.py
@@ -1,12 +1,14 @@
 # Create your views here.
+import os
 import json
 
 from django.http import HttpResponse
 
 def resource_listing(request):
     #return HttpResponse('1', mimetype='application/json')
-    return HttpResponse(json.dumps({
-        "apiVersion": "1.0.0",
-        "swaggerVersion": "1.2",
-        "apis": [],
-    }), mimetype='application/json')
+    CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
+    print "Current path = " + CURRENT_PATH
+    json_data = open(CURRENT_PATH + '/resource.json')
+    #data1 = json.load(json_data) // deserialises it
+    #data2 = json.dumps(json_data)  #json formatted string
+    return HttpResponse(json_data, mimetype='application/json')
